8362a4a3233639d04ba525fdd5a34cce5bf3f12c
Balázs Keresztury <b@belidzs.hu>
2019-05-25 21:18:47 +0200

    Fixed AssemblyTitle() once again

--- BankAccount/Properties/AssemblyInfo.cs
+++ BankAccount/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MagyarNemzetiBank.Properties")]
+[assembly: AssemblyTitle("MagyarNemzetiBank.BankAccount")]
 [assembly: AssemblyDescription("This library written in pure C# checks the validity of Hungarian bank account numbers and determines which bank they belong to.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Balázs Keresztury")]
