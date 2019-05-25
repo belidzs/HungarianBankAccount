719251cc14e996b628a44e9c5a57f7917e76717e
Balázs Keresztury <b@belidzs.hu>
2019-05-25 18:31:14 +0200

    Added AssemblyInfo.cs

---
+++ BankAccount/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MagyarNemzetiBank.Properties")]
+[assembly: AssemblyDescription("This library written in pure C# checks the validity of Hungarian bank account numbers and determines which bank they belong to.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Balázs Keresztury")]
+[assembly: AssemblyProduct("MagyarNemzetiBank.Properties")]
+[assembly: AssemblyCopyright("Copyright (c) 2019 Balázs Keresztury")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("46844df6-193c-425b-b616-7e659dee4fb0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
